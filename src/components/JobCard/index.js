9ed0d41a6aa4274d316bddae4468bea439c85dc3
Felipe de Paula <emilcyfelipe@gmail.com>
2022-08-27 23:32:32 -0300

    jobcard

--- src/components/JobCard/index.js
+++ src/components/JobCard/index.js
@@ -1,37 +1,56 @@
 import React, {useState, useEffect} from 'react';
+
 import {View, Text} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 import LogoImg from '../../assets/job_standard.png';
+
 import {
+  ContainerShadow,
   Container,
   HeadWrapper,
   Logo,
+  HeadDescription,
   Title,
   CompanyName,
   WorkRelationship,
+  Open,
+  Split,
   LocaleWrapper,
   Locale,
   LaunchedDate,
   Description,
+  TimeLapse,
 } from './styles';
 
 export default function JobCard() {
   return (
-    <Container>
-      <HeadWrapper>
-        <Logo source={LogoImg} />
-        <Title>UI/UX Designer Junior</Title>
-        <CompanyName>Torchinha Inc.</CompanyName>
-        <WorkRelationship></WorkRelationship>
-      </HeadWrapper>
-      <LocaleWrapper>
-        <Locale>São Paulo Capital, São Paulo, Brasil</Locale>
-        <LaunchedDate>20/02/2022</LaunchedDate>
-      </LocaleWrapper>
-      <Description>
-        Procuramos um profissional para ocupar a posição de UX Designer em um
-        cliente referência no setor de produção de imunobiológicos (vacinas) e
-        análises laboratoriais veterinários Ver mais...
-      </Description>
-    </Container>
+    <ContainerShadow>
+      <Container>
+        <HeadWrapper>
+          <Logo source={LogoImg} />
+          <HeadDescription>
+            <Title>UI/UX Designer Junior</Title>
+            <CompanyName>Torchinha Inc.</CompanyName>
+            <WorkRelationship>Junior . CLT</WorkRelationship>
+          </HeadDescription>
+        </HeadWrapper>
+        <Open>
+          <Icon name="open-outline" size={25} color="#BFBFBF" />
+        </Open>
+        <Split />
+        <LocaleWrapper>
+          <Locale>São Paulo Capital, São Paulo, Brasil</Locale>
+
+          <LaunchedDate>20/02/2022</LaunchedDate>
+        </LocaleWrapper>
+        <Description>
+          Procuramos um profissional para ocupar a posição de UX Designer em um
+          cliente referência no setor de produção de imunobiológicos (vacinas) e
+          análises laboratoriais veterinários Ver mais...
+        </Description>
+        <TimeLapse>Há 22h</TimeLapse>
+      </Container>
+    </ContainerShadow>
   );
 }
--- src/components/JobCard/styles.js
+++ src/components/JobCard/styles.js
@@ -1,21 +1,79 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const ContainerShadow = styled.View`
+  border-radius: 5px;
+  background-color: transparent;
+  overflow: hidden;
+  margin: 10px;
+  elevation: 4;
+`;
 
-export const HeadWrapper = styled.View``;
+export const Container = styled.View`
+  background-color: #fff;
+  padding: 20px;
+  position: relative;
+`;
 
-export const Logo = styled.Image``;
+export const HeadWrapper = styled.View`
+  flex-direction: row;
+`;
 
-export const Title = styled.Text``;
+export const Logo = styled.Image`
+  width: 57px;
+  height: 57px;
+`;
 
-export const CompanyName = styled.Text``;
+export const HeadDescription = styled.View`
+  margin-left: 10px;
+`;
 
-export const WorkRelationship = styled.Text``;
+export const Title = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+`;
 
-export const LocaleWrapper = styled.View``;
+export const CompanyName = styled.Text`
+  font-size: 12px;
+`;
 
-export const Locale = styled.Text``;
+export const WorkRelationship = styled.Text`
+  font-size: 10px;
+`;
+export const Open = styled.TouchableOpacity`
+  position: absolute;
+  right: 20px;
+  top: 20px;
+`;
 
-export const LaunchedDate = styled.Text``;
+export const Split = styled.View`
+  height: 2px;
+  width: 100%;
+  background-color: #e8e8e8;
+  margin: 15px 0 12px 0;
+`;
 
-export const Description = styled.Text``;
+export const LocaleWrapper = styled.View`
+  flex-direction: row;
+  margin-bottom: 6px;
+`;
+
+export const Locale = styled.Text`
+  font-size: 10px;
+`;
+
+export const LaunchedDate = styled.Text`
+  margin-left: 5px;
+  font-size: 10px;
+`;
+
+export const Description = styled.Text`
+  font-size: 11px;
+  margin-bottom: 20px;
+`;
+
+export const TimeLapse = styled.Text`
+  position: absolute;
+  font-size: 10px;
+  right: 20px;
+  bottom: 20px;
+`;
