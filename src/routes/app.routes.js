625e3127b51c3fe8c155e12650830a38f55f79de
Felipe de Paula <emilcyfelipe@gmail.com>
2022-08-29 22:36:31 -0300

    tab_icons

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import Home from '../pages/Home';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Home from '../pages/Home';
+import Jobs from '../pages/Jobs';
+import Location from '../pages/Location';
+import Profile from '../pages/Profile';
+import Register from '../pages/Register';
 const Tab = createBottomTabNavigator();
 
 export default function AppRoutes() {
   return (
     <Tab.Navigator
       screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
+        headerShown: false,
+        tabBarIcon: ({focused}) => {
           let iconName;
           if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Location') {
-            iconName = focused ? 'location' : 'location-outline';
-          } else if (route.name === 'Add') {
-            iconName = focused ? 'add' : 'add-outline';
-          } else if (route.name === 'Profile') {
-            iconName = focused ? 'person' : 'person-outline';
+            iconName = 'home-outline';
           } else if (route.name === 'Jobs') {
-            iconName = focused ? 'bag' : 'bag-outline';
+            iconName = 'ios-briefcase-outline';
+          } else if (route.name === 'Location') {
+            iconName = 'ios-location-outline';
+          } else if (route.name === 'Profile') {
+            iconName = 'ios-person-outline';
+          } else if (route.name === 'Register') {
+            iconName = 'add-circle-outline';
           }
-          return <Icon name={iconName} size={25} />;
+          return (
+            <Ionicons
+              name={iconName}
+              color={focused ? '#1165BA' : '#898989'}
+              size={30}
+            />
+          );
         },
       })}>
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Location" component={Location} />
+      <Tab.Screen name="Register" component={Register} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Jobs" component={Jobs} />
     </Tab.Navigator>
   );
 }
