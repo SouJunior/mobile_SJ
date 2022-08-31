5e42016e994bbbd6332cd9afb6cb05b8c9320c38
Felipe de Paula <emilcyfelipe@gmail.com>
2022-08-31 01:58:42 -0300

    dismiss keyboard

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList, Image} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+import {Container} from './styles';
 
 import Header from '../../components/Header';
 import JobCard from '../../components/JobCard';
@@ -11,22 +20,24 @@ export default function Home() {
   const [response, setResponse] = useState([1, 2, 3, 4, 5]);
 
   return (
-    <View>
-      <Header />
-      {response ? (
-        <FlatList data={response} renderItem={() => <JobCard />} />
-      ) : (
-        <View
-          style={{
-            width: '100%',
-            height: '100%',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#fff',
-          }}>
-          <Image source={Bg} style={{width: 150, height: 150}} />
-        </View>
-      )}
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Container>
+        <Header />
+        {response ? (
+          <FlatList data={response} renderItem={() => <JobCard />} />
+        ) : (
+          <View
+            style={{
+              width: '100%',
+              height: '100%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#fff',
+            }}>
+            <Image source={Bg} style={{width: 150, height: 150}} />
+          </View>
+        )}
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
